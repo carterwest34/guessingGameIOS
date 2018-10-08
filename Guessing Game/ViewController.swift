040e384de4aa80d3f41db87deedd48b71afc735c
Carter West <carterwest@macbook-pro-67.local>
2018-10-08 13:18:10 -0500

    Initial Commit

---
+++ Guessing Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guessing Game
+//
+//  Created by Carter West on 10/8/18.
+//  Copyright © 2018 Carter West. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
